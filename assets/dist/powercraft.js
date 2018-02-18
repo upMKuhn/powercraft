a38fe9751a177238d8ede2a329944411336b3fb9
Martin <up687791@myport.ac.uk>
2018-02-18 16:36:40 +0100

    update bundle build

--- assets/dist/powercraft.js
+++ assets/dist/powercraft.js
@@ -1,5 +1,7 @@
+/*
 var host = window.location.host.replace(':'+window.location.port,'');
 
 $.getScript('//'+host + ':35729/livereload.js', function(){
     console.log('livereoload enabled');
 });
+*/
