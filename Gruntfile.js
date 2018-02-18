076c577889b796a3aaab3e9e598a61d5ded10e3a
Martin <up687791@myport.ac.uk>
2018-02-18 15:30:40 +0100

    refactored folders

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,9 +14,9 @@ module.exports = function(grunt) {
       },
       styles: {
         files: [{
-          expand: true,
-          src: ['scss/**/*.scss', 'bower_components/bootstrap/scss/**/*.scss'],
-          dest: 'dist/powercraft',
+          expand: false,
+          src: ['assets/scss/powercraft.scss'],
+          dest: 'assets/dist/powercraft.min.css',
           ext: '.css'
         }]
       }
@@ -31,16 +31,9 @@ module.exports = function(grunt) {
         footer: '\n',
         banner: '\n',
         src: [
-          'js/**/*.js',
+          'assets/js/**/*.js',
       ],
-        dest: 'dist/powercraft.js',
-      },
-
-      css: {
-        src: [
-          'dist/powercraft/**/*.css',
-      ],
-        dest: 'dist/powercraft.min.css',
+        dest: 'assets/dist/powercraft.js',
       },
 
       jsDependencies: {
@@ -51,13 +44,7 @@ module.exports = function(grunt) {
           'bower_components/tether/dist/js/tether.min.js',
           'bower_components/bootstrap/dist/js/bootstrap.min.js',
       ],
-        dest: 'dist/powercraft-dependencies.min.js',
-      },
-      cssDependencies: {
-        src: [
-          'bower_components/tether/dist/css/tether.min.css',
-      ],
-        dest: 'dist/powercraft-dependencies.min.css',
+        dest: 'assets/dist/includes.min.js',
       },
     },
     
@@ -68,15 +55,15 @@ module.exports = function(grunt) {
       },
       my_target: {
         files: {
-          'dist/powercraft.min.js': ['dist/powercraft.js'],
+          'assets/dist/powercraft.min.js': ['assets/dist/powercraft.js'],
         }
       }
     },
 
     watch: {
       mywatch: {
-        files: ['scss/**/*.scss', 'js/**/*.js', '**/*.html', "!_site/**/*.*"],
-        tasks: ['sass', 'concat', 'uglify'],
+        files: ['assets/**/*.*'],
+        tasks: ['sass', 'concat'],
         options: {
           interrupt: true,
           spawn: true,
